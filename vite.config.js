6a3e38fe265bd6320eb5fc126d1e8d0ffa5f90d1
yunamail8 <yunamail8@gmail.com>
2026-03-07 17:26:19 +0800

    build: 嘗試使用 terser 並優化打包配置

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/college/',
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          framer: ['framer-motion']
+          'react-vendor': ['react', 'react-dom'],
+          'framer-motion': ['framer-motion']
         }
       }
     }
